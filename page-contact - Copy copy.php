2412f494eff659ff325c36f57e452fbd981e5a4b
bolt9999 <truedev1001@gmail.com>
2023-11-20 18:38:51 +0900

    update

---
+++ page-contact - Copy copy.php
@@ -0,0 +1,24 @@
+<?php get_header('service'); ?>
+    <main class="cc_main">
+        <div class="contact_main_sp">
+            <h1>お問い合わせ</h1>
+        </div>
+        <div class="contact_main">
+            <h1>お問い合わせ</h1>
+        </div>
+        <div class="tel_area">
+            <h2>お電話でのお問い合わせ</h2>
+            <a href="tel:0573722331"><span>0573-72-2331</span></a>
+            <p>受付時間：平日8:15～17:20</p>
+        </div>
+        <div class="mail_area">
+            <h2>メールでのお問い合わせ</h2>
+            <p>メールは24時間受け付けております。お問い合わせ後、3営業日以内にご連絡いたします。<br>
+            お問い合わせいただく内容およびお客様の個人情報は、弊社の個人情報保護方針に則り管理いたします。<wbr>
+            詳しくは「<a href="https://union-denki.co.jp/purchase/privacy/">個人情報の取扱いについて</a>」のページをご覧ください。<wbr>
+            お問い合わせフォームをご使用の場合は、個人情報保護方針の内容にご了承の上で送信いただきますようお願い申し上げます。</p>
+        </div>
+        <p class="pagetop"><a href="#wrap">∧</a></p>
+        <?php echo apply_shortcodes( '[contact-form-7 id="6fb5130" title="お問い合わせ"]' ); ?>
+    </main>
+    <?php get_footer('service'); ?>
